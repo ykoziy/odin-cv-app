a04810f3dbbdced671c67f69fa235a5ac2aca8e9
Yuriy Koziy <yukoziy@gmail.com>
2022-04-09 13:00:25 -0400

    fixed main app styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import Personal from './components/Personal';
 import WorkExperience from './components/WorkExperience/WorkExperience';
 import Education from './components/Education/Education';
+import styles from './styles/App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <main>
+      <main className={styles['app-container']}>
         <Personal />
         <WorkExperience />
         <Education />
