bc793babd74284d5884ce902d685d5921f640b8f
Yuriy Koziy <yukoziy@gmail.com>
2022-04-09 11:54:45 -0400

    Added ability to change full name and current position.

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -2,12 +2,102 @@ import React, { Component } from 'react';
 import styles from '../styles/Personal.module.css';
 
 class Personal extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      fullName: 'John Doe',
+      currentRole: 'Current Position',
+      editField: null,
+    };
+    this.handleInputFocus = this.handleInputFocus.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleRoleChange = this.handleRoleChange.bind(this);
+    this.editFieldHandler = this.editFieldHandler.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  handleInputFocus(e) {
+    e.target.select();
+  }
+
+  handleNameChange(e) {
+    this.setState({
+      fullName: e.target.value,
+    });
+  }
+
+  handleRoleChange(e) {
+    this.setState({
+      currentRole: e.target.value,
+    });
+  }
+
+  editFieldHandler(fieldName) {
+    this.setState({
+      editField: fieldName,
+    });
+  }
+
+  renderFullNameField() {
+    const { editField, fullName } = this.state;
+    if (editField === 'full-name') {
+      return (
+        <input
+          type="text"
+          value={fullName}
+          onChange={this.handleNameChange}
+          onFocus={this.handleInputFocus}
+          autoFocus
+        ></input>
+      );
+    } else {
+      return (
+        <h1 onClick={() => this.editFieldHandler('full-name')}>{fullName}</h1>
+      );
+    }
+  }
+
+  renderCurrentRole() {
+    const { editField, currentRole } = this.state;
+    if (editField === 'current-position') {
+      return (
+        <input
+          type="text"
+          value={currentRole}
+          onChange={this.handleRoleChange}
+          onFocus={this.handleInputFocus}
+          autoFocus
+        ></input>
+      );
+    } else {
+      return (
+        <h2 onClick={() => this.editFieldHandler('current-position')}>
+          {currentRole}
+        </h2>
+      );
+    }
+  }
+
+  handleClickOutside(e) {
+    if (e.target.nodeName !== 'INPUT') {
+      this.setState({
+        editField: 0,
+      });
+    }
+  }
+
   render() {
+    const { editField } = this.state;
+    if (editField) {
+      document.addEventListener('mousedown', this.handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', this.handleClickOutside);
+    }
     return (
       <section className={styles.personal}>
         <div className={styles['left-container']}>
-          <h1>John Doe</h1>
-          <h2>Current Position</h2>
+          {this.renderFullNameField()}
+          {this.renderCurrentRole()}
         </div>
         <div className={styles['right-container']}>
           <p>location</p>
