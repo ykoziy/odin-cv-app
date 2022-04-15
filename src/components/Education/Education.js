b91911350d92efe73e5a376d3fddd82a7e2eebec
Yuriy Koziy <yukoziy@gmail.com>
2022-04-15 15:04:33 -0400

    Education now renders from state

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,22 +1,50 @@
 import React, { Component } from 'react';
 import styles from '../../styles/Education.module.css';
 import EducationEntry from './EducationEntry';
+import uniqid from 'uniqid';
 
 class Education extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      education: [
+        {
+          key: uniqid(),
+          title: 'education title 1',
+          schoolName: 'school 1',
+          startDate: 'start date',
+          endDate: 'end date',
+        },
+        {
+          key: uniqid(),
+          title: 'education title 2',
+          schoolName: 'school 2',
+          startDate: 'start date',
+          endDate: 'end date',
+        },
+      ],
+    };
+  }
+
+  renderEntry(education) {
+    return (
+      <EducationEntry
+        key={education.key}
+        title={education.title}
+        schoolName={education.schoolName}
+        dates={`${education.startDate} - ${education.endDate}`}
+      />
+    );
+  }
+
   render() {
+    const educationList = this.state.education.map((education) =>
+      this.renderEntry(education),
+    );
     return (
       <section className={styles.education}>
         <h2>Education</h2>
-        <EducationEntry
-          title="education title 1"
-          schoolName="school 1"
-          dates="start date - end date"
-        />
-        <EducationEntry
-          title="education title 2"
-          schoolName="school 2"
-          dates="start date - end date"
-        />
+        {educationList}
       </section>
     );
   }
