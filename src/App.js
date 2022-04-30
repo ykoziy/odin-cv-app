b0e4734cd1593f41d054336fb5a383d7ff98c0b4
Yuriy Koziy <yukoziy@gmail.com>
2022-04-30 16:36:32 -0400

    Converted App to functional component

--- src/App.js
+++ src/App.js
@@ -1,67 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Personal from './components/Personal';
 import WorkExperience from './components/WorkExperience/WorkExperience';
 import Education from './components/Education/Education';
 import Modal from './components/Modal/Modal';
 import styles from './styles/App.module.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [isEditingCv, setIsEditingCv] = useState(true);
+  const [showModal, setShowModal] = useState(true);
 
-    this.state = { isEditingCv: true, showModal: true };
-    this.onClickEdit = this.onClickEdit.bind(this);
-    this.onClickView = this.onClickView.bind(this);
-    this.onCloseModal = this.onCloseModal.bind(this);
-  }
+  const onClickEdit = () => {
+    setIsEditingCv(true);
+  };
 
-  onClickEdit() {
-    this.setState({ isEditingCv: true });
-  }
+  const onClickView = () => {
+    setIsEditingCv(false);
+  };
 
-  onClickView() {
-    this.setState({ isEditingCv: false });
-  }
+  const onCloseModal = () => {
+    setShowModal(false);
+  };
 
-  onCloseModal() {
-    this.setState({ showModal: false });
-  }
-
-  render() {
-    const isEditingCv = this.state.isEditingCv;
-    return (
-      <React.Fragment>
-        <Modal
-          modalType="welcome"
-          isOpen={this.state.showModal}
-          closeModalHandler={this.onCloseModal}
-        />
-        <header>
-          <h1>CV Builder Application</h1>
-          <div className={styles.buttons}>
-            <button
-              onClick={this.onClickEdit}
-              disabled={isEditingCv ? true : false}
-            >
-              Edit mode
-            </button>
-            <button
-              onClick={this.onClickView}
-              disabled={isEditingCv ? false : true}
-            >
-              View mode
-            </button>
-            <div className={styles['button-divider']}></div>
-          </div>
-        </header>
-        <main className={styles['app-container']}>
-          <Personal isEditingCv={isEditingCv} />
-          <WorkExperience isEditingCv={isEditingCv} />
-          <Education isEditingCv={isEditingCv} />
-        </main>
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      <Modal
+        modalType="welcome"
+        isOpen={showModal}
+        closeModalHandler={onCloseModal}
+      />
+      <header>
+        <h1>CV Builder Application</h1>
+        <div className={styles.buttons}>
+          <button onClick={onClickEdit} disabled={isEditingCv ? true : false}>
+            Edit mode
+          </button>
+          <button onClick={onClickView} disabled={isEditingCv ? false : true}>
+            View mode
+          </button>
+          <div className={styles['button-divider']}></div>
+        </div>
+      </header>
+      <main className={styles['app-container']}>
+        <Personal isEditingCv={isEditingCv} />
+        <WorkExperience isEditingCv={isEditingCv} />
+        <Education isEditingCv={isEditingCv} />
+      </main>
+    </React.Fragment>
+  );
+};
 
 export default App;
