209f76cfa528ad41218ccff5330f3da7a92ef979
Yuriy Koziy <yukoziy@gmail.com>
2022-04-30 16:44:21 -0400

    Converted EducationEntry to functional component

--- src/components/Education/EducationEntry.js
+++ src/components/Education/EducationEntry.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareMinus, faSquarePen } from '@fortawesome/free-solid-svg-icons';
 import sharedStyles from '../../styles/SharedStyles.module.css';
 import styles from '../../styles/EducationEntry.module.css';
 
-class EducationEntry extends Component {
-  constructor(props) {
-    super();
-  }
-
-  render() {
-    const {
-      title,
-      schoolName,
-      dates,
-      index,
-      onDeleteEntry,
-      onEditEntry,
-      isEditingCv,
-    } = this.props;
-
-    let editElements;
+const EducationEntry = ({
+  title,
+  schoolName,
+  dates,
+  index,
+  onDeleteEntry,
+  onEditEntry,
+  isEditingCv,
+}) => {
+  const renderEditButtons = () => {
     if (isEditingCv) {
-      editElements = (
+      return (
         <React.Fragment>
           <button
             className={sharedStyles['delete-btn']}
@@ -36,20 +29,19 @@ class EducationEntry extends Component {
         </React.Fragment>
       );
     }
-
-    return (
-      <article className={styles.container} index={index}>
-        <div className={styles['left-container']}>
-          <p className={sharedStyles['entry-title']}>{title}</p>
-          {editElements}
-        </div>
-        <div className={styles['right-container']}>
-          <p>{schoolName}</p>
-          <p>{dates}</p>
-        </div>
-      </article>
-    );
-  }
-}
+  };
+  return (
+    <article className={styles.container} index={index}>
+      <div className={styles['left-container']}>
+        <p className={sharedStyles['entry-title']}>{title}</p>
+        {renderEditButtons()}
+      </div>
+      <div className={styles['right-container']}>
+        <p>{schoolName}</p>
+        <p>{dates}</p>
+      </div>
+    </article>
+  );
+};
 
 export default EducationEntry;
