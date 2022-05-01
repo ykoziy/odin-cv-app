a8ec6f24cd8e52ce2fe47cbe1c9ca27671d91efa
Yuriy Koziy <yukoziy@gmail.com>
2022-04-30 21:51:04 -0400

    Converted Personal to functional component

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faEnvelope,
@@ -9,195 +9,172 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/Personal.module.css';
 
-class Personal extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      fullName: 'John Doe',
-      currentRole: 'Current Position',
-      location: 'New York, NY',
-      phone: '212-777-777',
-      email: 'johndoe@domain.com',
-      linkedIn: 'linkedIn link',
-      about: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-      aliquip ex ea commodo consequat. Duis aute irure dolor in
-      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-      pariatur.`,
-      editField: null,
-    };
-    this.handleInputFocus = this.handleInputFocus.bind(this);
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleRoleChange = this.handleRoleChange.bind(this);
-    this.handleAboutChange = this.handleAboutChange.bind(this);
-    this.handleGeneralFieldChange = this.handleGeneralFieldChange.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.editFieldHandler = this.editFieldHandler.bind(this);
-    this.handleClickOutside = this.handleClickOutside.bind(this);
-  }
+const Personal = ({ isEditingCv }) => {
+  const [fullName, setFullName] = useState('John Doe');
+  const [currentRole, setCurrentRole] = useState('Current Position');
+  const [location, setLocation] = useState('New York, NY');
+  const [phone, setPhone] = useState('212-777-777');
+  const [email, setEmail] = useState('johndoe@domain.com');
+  const [linkedIn, setLinkedIn] = useState('linkedIn link');
+  const [about, setAbout] =
+    useState(`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+  aliquip ex ea commodo consequat. Duis aute irure dolor in
+  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+  pariatur.`);
+  const [editField, setEditField] = useState(null);
 
-  handleInputFocus(e) {
+  useEffect(() => {
+    if (editField) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  });
+
+  const handleInputFocus = (e) => {
     e.target.select();
-  }
+  };
 
-  handleNameChange(e) {
-    this.setState({
-      fullName: e.target.value,
-    });
-  }
+  const handleNameChange = (e) => {
+    setFullName(e.target.value);
+  };
 
-  handleRoleChange(e) {
-    this.setState({
-      currentRole: e.target.value,
-    });
-  }
+  const handleRoleChange = (e) => {
+    setCurrentRole(e.target.value);
+  };
 
-  handleGeneralFieldChange(e) {
-    const { editField } = this.state;
-    let stateName = null;
+  const handleGeneralFieldChange = (e) => {
+    let targetValue = e.target.value;
     if (editField === 'location') {
-      stateName = 'location';
+      setLocation(targetValue);
     } else if (editField === 'phone') {
-      stateName = 'phone';
+      setPhone(targetValue);
     } else if (editField === 'email') {
-      stateName = 'email';
+      setEmail(targetValue);
     } else if (editField === 'linkedin') {
-      stateName = 'linkedIn';
+      setLinkedIn(targetValue);
     }
+  };
 
-    if (stateName) {
-      this.setState({
-        [stateName]: e.target.value,
-      });
-    }
-  }
-
-  handleKeyDown(e) {
+  const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
-      this.handleBlankField(this.state.editField);
-      this.setState({
-        editField: null,
-      });
+      handleBlankField(editField);
+      setEditField(null);
     }
-  }
+  };
 
-  renderFullNameField() {
-    const { editField, fullName } = this.state;
+  const renderFullNameField = () => {
     if (editField === 'full-name') {
       return (
         <input
           type="text"
           value={fullName}
-          onChange={this.handleNameChange}
-          onFocus={this.handleInputFocus}
-          onKeyDown={this.handleKeyDown}
+          onChange={handleNameChange}
+          onFocus={handleInputFocus}
+          onKeyDown={handleKeyDown}
           autoFocus
         ></input>
       );
     } else {
-      return (
-        <h1 onClick={() => this.editFieldHandler('full-name')}>{fullName}</h1>
-      );
+      return <h1 onClick={() => editFieldHandler('full-name')}>{fullName}</h1>;
     }
-  }
+  };
 
-  renderCurrentRole() {
-    const { editField, currentRole } = this.state;
+  const renderCurrentRole = () => {
     if (editField === 'current-position') {
       return (
         <input
           type="text"
           value={currentRole}
-          onChange={this.handleRoleChange}
-          onFocus={this.handleInputFocus}
+          onChange={handleRoleChange}
+          onFocus={handleInputFocus}
+          onKeyDown={handleKeyDown}
           autoFocus
         ></input>
       );
     } else {
       return (
-        <h2 onClick={() => this.editFieldHandler('current-position')}>
+        <h2 onClick={() => editFieldHandler('current-position')}>
           {currentRole}
         </h2>
       );
     }
-  }
+  };
 
-  handleAboutChange(e) {
-    this.setState({
-      about: e.target.value,
-    });
-  }
+  const handleAboutChange = (e) => {
+    setAbout(e.target.value);
+  };
 
-  renderAboutSection() {
-    const { editField, about } = this.state;
+  const renderAboutSection = () => {
     if (editField === 'about') {
       return (
         <textarea
           type="text"
           value={about}
-          onChange={this.handleAboutChange}
-          onFocus={this.handleInputFocus}
+          onChange={handleAboutChange}
+          onFocus={handleInputFocus}
+          onKeyDown={handleKeyDown}
           autoFocus
           rows={4}
           cols={180}
         ></textarea>
       );
     } else {
-      return <p onClick={() => this.editFieldHandler('about')}>{about}</p>;
+      return <p onClick={() => editFieldHandler('about')}>{about}</p>;
     }
-  }
+  };
 
-  renderGeneralInput(value) {
+  const renderGeneralInput = (value) => {
     return (
       <input
         type="text"
         value={value}
-        onChange={this.handleGeneralFieldChange}
-        onFocus={this.handleInputFocus}
+        onChange={handleGeneralFieldChange}
+        onFocus={handleInputFocus}
+        onKeyDown={handleKeyDown}
         autoFocus
       ></input>
     );
-  }
+  };
 
-  renderGeneralPar(value, editFieldNme) {
-    return <p onClick={() => this.editFieldHandler(editFieldNme)}>{value}</p>;
-  }
+  const renderGeneralPar = (value, editFieldNme) => {
+    return <p onClick={() => editFieldHandler(editFieldNme)}>{value}</p>;
+  };
 
-  renderGeneralField(index) {
-    const { editField, location, phone, email, linkedIn } = this.state;
+  const renderGeneralField = (index) => {
     let element = null;
     let icon = null;
     switch (index) {
       case 0:
         if (editField === 'location') {
-          element = this.renderGeneralInput(location);
+          element = renderGeneralInput(location);
         } else {
-          element = this.renderGeneralPar(location, 'location');
+          element = renderGeneralPar(location, 'location');
         }
         icon = faCity;
         break;
       case 1:
         if (editField === 'phone') {
-          element = this.renderGeneralInput(phone);
+          element = renderGeneralInput(phone);
         } else {
-          element = this.renderGeneralPar(phone, 'phone');
+          element = renderGeneralPar(phone, 'phone');
         }
         icon = faPhone;
         break;
       case 2:
         if (editField === 'email') {
-          element = this.renderGeneralInput(email);
+          element = renderGeneralInput(email);
         } else {
-          element = this.renderGeneralPar(email, 'email');
+          element = renderGeneralPar(email, 'email');
         }
         icon = faEnvelope;
         break;
       case 3:
         if (editField === 'linkedin') {
-          element = this.renderGeneralInput(linkedIn);
+          element = renderGeneralInput(linkedIn);
         } else {
-          element = this.renderGeneralPar(linkedIn, 'linkedin');
+          element = renderGeneralPar(linkedIn, 'linkedin');
         }
         icon = faShareNodes;
         break;
@@ -212,90 +189,76 @@ class Personal extends Component {
         {element}
       </React.Fragment>
     );
-  }
+  };
 
-  editFieldHandler(fieldName) {
-    const { isEditingCv } = this.props;
+  const editFieldHandler = (fieldName) => {
     if (isEditingCv) {
-      this.setState({
-        editField: fieldName,
-      });
+      setEditField(fieldName);
     }
-  }
+  };
 
-  handleBlankField(fieldName) {
-    let stateName = null;
-    let stateValue = null;
+  const handleBlankField = (fieldName) => {
     if (fieldName === 'full-name') {
-      stateName = 'fullName';
-      stateValue = 'John Doe';
+      if (fullName === '') {
+        setFullName('John Doe');
+      }
     } else if (fieldName === 'current-position') {
-      stateName = 'currentRole';
-      stateValue = 'Current Position';
+      if (currentRole === '') {
+        setCurrentRole('Current Position');
+      }
     } else if (fieldName === 'location') {
-      stateName = 'location';
-      stateValue = 'New York, NY';
+      if (location === '') {
+        setLocation('New York, NY');
+      }
     } else if (fieldName === 'phone') {
-      stateName = 'phone';
-      stateValue = '212-777-7777';
+      if (phone === '') {
+        setPhone('212-777-7777');
+      }
     } else if (fieldName === 'email') {
-      stateName = 'email';
-      stateValue = 'johndoe@domain.com';
+      if (email === '') {
+        setEmail('johndoe@domain.com');
+      }
     } else if (fieldName === 'linkedin') {
-      stateName = 'linkedIn';
-      stateValue = 'linkedIn link';
+      if (linkedIn === '') {
+        setLinkedIn('linkedIn link');
+      }
     } else if (fieldName === 'about') {
-      stateName = 'about';
-      stateValue = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-      aliquip ex ea commodo consequat. Duis aute irure dolor in
-      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-      pariatur.`;
-    }
-    if (stateName && stateValue) {
-      if (this.state[stateName] === '') {
-        this.setState({
-          [stateName]: stateValue,
-        });
+      let about = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur.`;
+      if (about) {
+        setAbout(about);
       }
     }
-  }
+  };
 
-  handleClickOutside(e) {
+  const handleClickOutside = (e) => {
     if (e.target.nodeName !== 'INPUT' && e.target.nodeName !== 'TEXTAREA') {
-      this.handleBlankField(this.state.editField);
-      this.setState({
-        editField: null,
-      });
+      handleBlankField(editField);
+      setEditField(null);
     }
-  }
+  };
 
-  render() {
-    const { editField } = this.state;
-    if (editField) {
-      document.addEventListener('mousedown', this.handleClickOutside);
-    } else {
-      document.removeEventListener('mousedown', this.handleClickOutside);
-    }
-    return (
-      <section className={styles.personal}>
-        <div className={styles['left-container']}>
-          {this.renderFullNameField()}
-          {this.renderCurrentRole()}
-        </div>
-        <div className={styles['right-container']}>
-          <ul className={styles.list}>
-            <li>{this.renderGeneralField(0)}</li>
-            <li>{this.renderGeneralField(1)}</li>
-            <li>{this.renderGeneralField(2)}</li>
-            <li>{this.renderGeneralField(3)}</li>
-          </ul>
-        </div>
-        <div className={styles.about}>{this.renderAboutSection()}</div>
-      </section>
-    );
-  }
-}
+  return (
+    <section className={styles.personal}>
+      <div className={styles['left-container']}>
+        {renderFullNameField()}
+        {renderCurrentRole()}
+      </div>
+      <div className={styles['right-container']}>
+        <ul className={styles.list}>
+          <li>{renderGeneralField(0)}</li>
+          <li>{renderGeneralField(1)}</li>
+          <li>{renderGeneralField(2)}</li>
+          <li>{renderGeneralField(3)}</li>
+        </ul>
+      </div>
+      <div className={styles.about}>{renderAboutSection()}</div>
+    </section>
+  );
+};
 
 export default Personal;
